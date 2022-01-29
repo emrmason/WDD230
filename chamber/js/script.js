04199b1bf9a3f59eef2d50529dee8157e81acdf4
emrmason <mas20021@byui.edu>
2022-01-29 08:32:18 -0800

    Lots of upcates on Chamber page

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,2 +1,12 @@
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 document.getElementById('lastMod').textContent = new Date().toLocaleDateString('en-UK', options);
+
+const today = {month: 'long', day: 'numeric', year: 'numeric'};
+document.getElementById("currentDate").textContent = new Date().toLocaleDateString('en-us', options);
+
+const hambutton = document.querySelector('.ham');
+const mainnav = documnt.querySelector('.navigation');
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
