ee15531843c4967447ca621f18b8f02dfbdc2804
emrmason <mas20021@byui.edu>
2022-02-05 13:27:50 -0800

    Week 05 Assignment for Home Page

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -12,3 +12,18 @@ const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// banner toggle function
+
+function toggleBanner() {
+    if (options.weekday == "Monday") {
+        document.getElementById(".banner").style.display= "block";
+    }
+    if (options.weekday == "Tuesday") {
+        document.getElementById(".banner").style.display= "block";
+    }
+    else {
+        document.getElementById(".banner").style.display= "none";
+
+    }
+}
