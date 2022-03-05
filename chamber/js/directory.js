dabf9e70b30510c60fe628db45033f725e38e43e
emrmason <mas20021@byui.edu>
2022-03-05 13:10:11 -0800

    more Directory Updates

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -14,17 +14,24 @@ function displayMembers(member){
     let card= document.createElement('section');
     let h2 = document.createElement('h2');
     let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let p3 = document.createElement('p');
     let logo = document.createElement('img');
 
-    h2.innerHTML= `${member.name}`
-    p1.innerHTML= `Address: ${member.address}, Phone: ${member.phone}, Web: ${member.site}`;
+    h2.innerHTML= `${member.name}`;
+    p1.innerHTML= `Address: ${member.address}`;
+    p2.innerHTML = `Phone: ${member.phone}`;
+    p3.innerHTML = `Web: ${member.site}`;
     
     logo.setAttribute("src", member.logo);
     logo.setAttribute("alt", `${member.name} Logo`);
     logo.setAttribute("loading", "lazy");
+    card.setAttribute("class", "dirMembers");
 
     card.appendChild(h2);
     card.appendChild(p1);
+    card.appendChild(p2);
+    card.appendChild(p3);
     card.appendChild(logo);
 
     cards.appendChild(card);
