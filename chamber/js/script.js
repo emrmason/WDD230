9ce26502478bbf659b7a4be31514ed62fd7123f6
emrmason <mas20021@byui.edu>
2022-03-17 20:49:49 -0700

    Lots of changes.

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -19,16 +19,16 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
     const bannerday = new Date();
     const day = bannerday.getDay();
 
-    let banner = document.querySelector(".banner");
-    if (day == '1') {
-        banner.setAttribute("display", "block");
-    }
-    else if (day == '2') {
-        banner.setAttribute("display", "block");
-    }
-    else {
-        banner.setAttribute("display", "none");
-    }
+    const banner = document.getElementById("#banner");
+        if (day == '1') {
+            banner.setAttribute("display", "block");
+        }
+        else if (day == '2') {
+            banner.setAttribute("display", "block");
+        }
+        else {
+            banner.setAttribute("display", "none");
+        }
 
 
 const images = document.querySelectorAll("[data-src]");
@@ -68,13 +68,27 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5810988&units
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsonObject) => {
-    console.log(jsonObject);
     document.querySelector('#temp').textContent = jsonObject.main.temp;
     const iconsrc= `https://openweathermap.org/img/w/${jsonObject.weather[0].icon}.png`; 
-    const desc = (jsonObject.weather[0].description).toUpperCase();
+    const desc = jsonObject.weather[0].description;
+    // desc = desc.split(' ').map(capitalize).join(' ');
     const windspeed = jsonObject.wind.speed;
     document.querySelector('#weather-icon').setAttribute('src', iconsrc);
     document.querySelector('#weather-icon').setAttribute('alt', desc);
-    document.querySelector('#weatherDesc').textContent = desc; 
+    document.querySelector('#weatherDesc').textContent= desc; 
     document.querySelector('#speed').textContent = windspeed;
+
+    let t = jsonObject.main.temp;   
+    let wc = '';
+
+        if (t <= 50 && windspeed > 3) {
+        wc = parseFloat(35.74 + (0.6215 * t) - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * Math.pow(s,0.16))).toFixed(2);
+        document.getElementById('chill').innerHTML = `${wc} &#176;F`;}
+        else {
+        document.getElementById('chill').innerHTML= 'N/A';
+        }
   });
+
+// function capitalize(word) {
+//     return `${word.charAt(0).toUpperCase}${word.slice(1)}`;
+// }
